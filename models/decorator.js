7235eada6dd355b7a9adda0138e55da109633b05
Cody Abbott <cody.abbott16@gmail.com>
2020-01-07 16:04:17 +0000

    paint can constructor added to pass decorator test

--- models/decorator.js
+++ models/decorator.js
@@ -1,3 +1,10 @@
 const Decorator = function(paintCans){
   this.paintCans = [];
 };
+
+Decorator.prototype.addPaintToStock = function (paint) {
+  this.paintCans.push(paint);
+};
+
+
+module.exports = Decorator;
--- specs/painter_decorator_spec.js
+++ specs/painter_decorator_spec.js
@@ -6,9 +6,22 @@ const Decorator = require('../models/decorator');
 describe('Decorator', function(){
 
   let decorator;
+  let paintCan1;
 
   beforeEach(function(){
     decorator = new Decorator();
+    paintCan1 = new PaintCan(10);
+  })
+
+  it('should have no paint stock at start', function(){
+    const actual = decorator.paintCans.length;
+    assert.strictEqual(actual, 0);
+  });
+
+  it('should be able to add a can of paint to paint stock', function(){
+    decorator.addPaintToStock(paintCan1);
+    const actual = decorator.paintCans.length;
+    assert.strictEqual(actual, 1);
   })
 
 });
