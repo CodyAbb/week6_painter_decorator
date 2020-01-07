c3bcab7cda6e5c36edf591ed509572fc3e3d31b9
Cody Abbott <cody.abbott16@gmail.com>
2020-01-07 15:54:22 +0000

    setup decorator constructor

---
+++ specs/painter_decorator_spec.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const Room = require('../models/room');
+const PaintCan = require('../models/paint_can');
+const Decorator = require('../models/decorator');
+
+describe('Decorator', function(){
+
+  let decorator;
+
+  beforeEach(function(){
+    decorator = new Decorator();
+  })
+
+});
